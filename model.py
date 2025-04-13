0d134fc7a4be85839f06764250ee7aaa82815add
mrbane10 <ikaisar10@gmail.com>
2025-04-14 00:17:18 +0530

    model

--- model.py
+++ model.py
@@ -48,4 +48,19 @@ class PositionalEncoding(nn.Module):
 class LayerNormalization(nn.Module):
        
        def __init__(self,eps:float=10**-6):
+              super().__init__()
+              self.eps=eps
+              self.alpha=nn.Parameter(torch.ones(1)) # Multiplied 
+              self.bias=nn.Parameter(torch.zeros(1)) # Added
+
+       def forward(self,x):
+              mean=x.mean(dim = -1,keepdim=True)
+              std=x.std(dim = -1, keepdim=True)
+              return self.alpha * (x - mean) / (std + self.eps) + self.bias
+                
+
+
+
+              
+              
               
